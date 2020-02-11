2e809113a0be270a7f34415d314564f6d78862fa
Ari Wasch <ariwasch@gmail.com>
2020-02-11 09:12:28 -0500

    Initial Commit

---
+++ Bluetooth Digital Keypad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bluetooth Digital Keypad
+//
+//  Created by Ari Wasch on 2/11/20.
+//  Copyright © 2020 Ari Wasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bluetooth Digital KeypadTests/Bluetooth_Digital_KeypadTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bluetooth_Digital_KeypadTests.swift
+//  Bluetooth Digital KeypadTests
+//
+//  Created by Ari Wasch on 2/11/20.
+//  Copyright © 2020 Ari Wasch. All rights reserved.
+//
+
+import XCTest
+@testable import Bluetooth_Digital_Keypad
+
+class Bluetooth_Digital_KeypadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
